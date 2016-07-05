7119b391467e749d9ba80fe98fd5f8fd46a9dfea
sakura.davis <you@yourdomain.com>
2016-07-04 21:24:45 -0400

    Built Timeline Query.

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -10,17 +10,54 @@ import UIKit
 import Parse
 
 class TimelineViewController: UIViewController {
-    //Create an instance of a PhotoTakingHelper that will display our popup.
-    var photoTakingHelper: PhotoTakingHelper?
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //TIMELINE QUERY (SHOW POSTS)
+    var posts: [Post] = []
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //Creating the query that fetches the Follow relationships for the current user.
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        
+        //use that query to fetch any posts that are created by users that the current user is following.
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        // another query to retrieve all posts that the current user has posted.
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        //create a combined query of the 2. and 3. queries using the orQueryWithSubqueries method.
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        //Know user for all posts
+        query.includeKey("user")
+        //chronological order.
+        query.orderByDescending("createdAt")
+        
+        //We kick off the network request.
+        query.findObjectsInBackgroundWithBlock {(result: [PFObject]?, error: NSError?) -> Void in
+            //Cast result [PFObject] to [Post] or store an empty array ([]) in self.posts.
+            self.posts = result as? [Post] ?? []
+            // Refresh
+            self.tableView.reloadData()
+        }
+    }
     
     
     //Setting the TimelineViewController to be the delegate of the tabBarController.
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.tabBarController?.delegate = self
     }
+    
+    //PHOTO TAKING
+    //Create an instance of a PhotoTakingHelper that will display our popup.
+    var photoTakingHelper: PhotoTakingHelper?
+
     
     func takePhoto() {
         //Within the takePhoto method we're creating an instance of PhotoTakingHelper. We're assigning that instance to the photoTakingHelper property. The initializer of the PhotoTakingHelper takes two parameters: the view controller on which the popup should be presented and the callback that should run as soon as a photo has been selected
@@ -38,7 +75,14 @@ class TimelineViewController: UIViewController {
         )
     }
     
+    
+    
+    
 }
+
+
+
+
 
 // MARK: Tab Bar Delegate
 //Using this method the tab bar view controller asks its delegate whether or not it should present the view controller that belongs to the tab bar item that the user just tapped.
@@ -56,5 +100,22 @@ func tabBarController(tabBarController: UITabBarController, shouldSelectViewCont
         else {
             return true
         }
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //Our Table View needs to have as many rows as we have posts stored in the posts property
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        //For now we return a simple placeholder cell with the title "Post" 
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell")!
+        
+        cell.textLabel!.text = "Post"
+        
+        return cell
     }
 }
