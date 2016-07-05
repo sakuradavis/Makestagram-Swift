f62dad0129e3d3939a823b8c6dbfe282bf451ce2
sakura.davis <you@yourdomain.com>
2016-07-04 23:34:09 -0400

    Decorated post cells.

--- Makestagram/Timeline/PostTableViewCell.swift
+++ Makestagram/Timeline/PostTableViewCell.swift
@@ -12,6 +12,18 @@ import Bond
 class PostTableViewCell: UITableViewCell {
 
     @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var likesIconImageView: UIImageView!
+    @IBOutlet weak var likesLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+     @IBOutlet weak var moreButton: UIButton!
+    
+    @IBAction func moreButtonTapped(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func likeButtonTapped(sender: AnyObject) {
+        
+    }
     
     //property and property observer
     var post: Post? {
