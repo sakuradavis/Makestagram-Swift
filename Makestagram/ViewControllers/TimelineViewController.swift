3a8b720aa449aa94e5a51d5c51f5ed96e0aa1341
sakura.davis <you@yourdomain.com>
2016-07-05 11:49:48 -0400

    use the TimelineComponent! to load posts as you reach the bottom.

--- Makestagram/ViewControllers/TimelineViewController.swift
+++ Makestagram/ViewControllers/TimelineViewController.swift
@@ -8,47 +8,51 @@
 
 import UIKit
 import Parse
+import ConvenienceKit
 
-class TimelineViewController: UIViewController {
+class TimelineViewController: UIViewController, TimelineComponentTarget {
     
     @IBOutlet weak var tableView: UITableView!
     
+    var timelineComponent: TimelineComponent<Post, TimelineViewController>!
+    
     //TIMELINE QUERY (SHOW POSTS)
-    var posts: [Post] = []
+    //var posts: [Post] = []
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
+        timelineComponent.loadInitialIfRequired()
+        //Following code removed after implementing TimelineComponent which takes care of recieving posts on timeline.
+//        //We no longer want to download all images immediately after the timeline query completes, instead we want to load them lazily as soon as a post is displayed.
+//        
+//        //We kick off the network request.
+//        ParseHelper.timelineRequestForCurrentUser {(result: [PFObject]?, error: NSError?) -> Void in
+//            
+//            //Cast result [PFObject] to [Post] or store an empty array ([]) in self.posts.
+//            self.posts = result as? [Post] ?? []
+//           
+////            //For all posts...
+////            for post in self.posts {
+////                do {
+////                    //download imageFile
+////                    let data = try post.imageFile?.getData()
+////                    //turn it from NSData into a UIImage, store in image property of post.
+////                    post.image = UIImage(data: data!, scale:1.0)
+////                } catch {
+////                    print("could not get image")
+////                }
+////            }
+//            
+//            //Refresh!
+//            self.tableView.reloadData()
         
-        //We no longer want to download all images immediately after the timeline query completes, instead we want to load them lazily as soon as a post is displayed.
-        
-        //We kick off the network request.
-        ParseHelper.timelineRequestForCurrentUser {
-            (result: [PFObject]?, error: NSError?) -> Void in
-            
-            //Cast result [PFObject] to [Post] or store an empty array ([]) in self.posts.
-            self.posts = result as? [Post] ?? []
-           
-//            //For all posts...
-//            for post in self.posts {
-//                do {
-//                    //download imageFile
-//                    let data = try post.imageFile?.getData()
-//                    //turn it from NSData into a UIImage, store in image property of post.
-//                    post.image = UIImage(data: data!, scale:1.0)
-//                } catch {
-//                    print("could not get image")
-//                }
-//            }
-            
-            //Refresh!
-            self.tableView.reloadData()
-        }
     }
-    
     
     //Setting the TimelineViewController to be the delegate of the tabBarController.
     override func viewDidLoad() {
         super.viewDidLoad()
+        //TimelineComponent only takes one argument when it's being initialized: the target. The target is the object to which the TimelineComponent shall add its functionality
+        timelineComponent = TimelineComponent(target: self)
         self.tabBarController?.delegate = self
     }
     
@@ -74,7 +78,22 @@ class TimelineViewController: UIViewController {
     }
     
     
+    //MARK: TimelineComponentTarget Protocol!!!
+    //initially show the newest 5 posts
+    let defaultRange = 0...4
+    //When the user reaches the end of the timeline, we load an additional 5.
+    let additionalRangeSize = 5
     
+    //implement the loadInRange method so that the TimelineComponent can call it and receive the posts on a user's timeline.
+    func loadInRange(range: Range<Int>, completionBlock: ([Post]?) -> Void) {
+        //pass the range
+        ParseHelper.timelineRequestForCurrentUser(range) { (result: [PFObject]?, error: NSError?) -> Void in
+            // In the callback of the query we check whether or not we have received a result. If the result is nil we store an empty array in the posts variable.
+            let posts = result as? [Post] ?? []
+            //pass the posts that have been loaded back to the TimelineComponent by calling the completionBlock.
+            completionBlock(posts)
+        }
+    }
     
 }
 
@@ -103,14 +122,13 @@ extension TimelineViewController: UITableViewDataSource {
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //Our Table View needs to have as many rows as we have posts stored in the posts property
-        return posts.count
+        return timelineComponent.content.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         //cast cell to our custom class PostTableViewCell
         let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
-        let post = posts[indexPath.row]
-        
+        let post = timelineComponent.content[indexPath.row]        
         // Directly before a post will be displayed, we trigger the image download.
         post.downloadImage()
         //fetch likes
@@ -125,4 +143,14 @@ extension TimelineViewController: UITableViewDataSource {
 //        cell.postImageView.image = posts[indexPath.row].image
 //        return cell
     }
+}
+
+//Whenever a cell becomes visible, we are required to call the targetWillDisplayEntry: method and pass the index of the currently displayed cell. That way the TimelineComponent will know when the user has scrolled to the bottom of the timeline.
+extension TimelineViewController: UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        timelineComponent.targetWillDisplayEntry(indexPath.row)
+    }
+    
 }
